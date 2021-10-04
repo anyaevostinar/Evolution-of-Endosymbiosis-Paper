ef0ea21a4ce193e42fcd5631cbda43c98bf31c60
Kiara Johnson <kiara@oneotaslopes.org>
2021-10-04 01:54:17 -0500

    adding 100k updates description

---
+++ Data/10.03_ectosymbiosis_100k_reps/simple_repeat.py
@@ -0,0 +1,31 @@
+#a script to run several replicates of several treatments locally
+
+directory = "ecto_reps/rpts/" #set this to whichever directory you would like data files stored in
+seeds = range(1001,1031)
+vts = [0.0, 0.1, 0.2, 0.4, 0.5, 0.6, 0.8, 0.9, 1.0]
+ecto = [0, 1]
+ecto_im = [0, 1]
+import subprocess
+
+def cmd(command):
+    '''This wait causes all executions to run in sieries.
+    For parralelization, remove .wait() and instead delay the
+    R script calls unitl all neccesary data is created.'''
+    return subprocess.Popen(command, shell=True).wait()
+
+def silent_cmd(command):
+    '''This wait causes all executions to run in sieries.
+    For parralelization, remove .wait() and instead delay the
+    R script calls unitl all neccesary data is created.'''
+    return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).wait()
+
+#needs executable ./symbulation
+
+for a in seeds:
+    for vt in vts:
+        for e in ecto:
+            for ei in ecto_im:
+                if ei == e: #we no longer care about immunity off
+		    command_str = './symbulation -ECTOSYMBIOSIS '+str(e)+' -ECTOSYMBIOTIC_IMMUNITY '+str(ei)+' -VERTICAL_TRANSMISSION '+str(vt)+' -SEED '+str(a)+' -START_MOI 1 -FILE_PATH '+directory+' -FILE_NAME SM1_Seed'+str(a)+'_VT'+str(vt)+'_ECTO'+str(e)+'_ECTOIM'+str(ei)
+                    print(command_str)
+                    cmd(command_str)
